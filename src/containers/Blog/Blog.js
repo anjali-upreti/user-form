7e81707d610a6246e955a7ea8c63bd0bfb1fa1d8
Anjali Upreti (Digital) <an321376@imgadmins-MacBook-Pro-2.local>
2018-10-18 11:18:42 +0530

    second commit

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import axios from 'axios';
 class Blog extends Component {
 
     state = {
-        posts: []
+        posts: [],
+        selectedPostId: null
     }
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts').then(response => 
@@ -25,10 +26,15 @@ class Blog extends Component {
         }
         );
     }
+    postSelectedHandler= (id)=> {
+        this.setState({selectedPostId: id})
+    }
     render () {
         const posts = this.state.posts.map(post => {
            
-            return <Post title= {post.title} key={post.id} author={post.author}/>
+            return <Post title= {post.title} key={post.id} 
+                    author={post.author}
+                    clicked={()=>this.postSelectedHandler(post.id)}/>
         })
         return (
             <div>
@@ -36,7 +42,7 @@ class Blog extends Component {
                     {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
